6a7ddeb38bc5f5be21f3bb73f0a3fed8d550386f
Shakya <varsha.shakya@capgemini.com>
2025-03-27 15:14:56 +0530

    Updated App.js and MyCustomComponent.js

--- src/MyCustomComponent.js
+++ src/MyCustomComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState , useEffect } from 'react';
 
 const fruits=[
     {name:'Guava', price:50 },
@@ -6,12 +7,40 @@ const fruits=[
     {name:'Grapes', price:120 }
 ];
 
-function myCustomComponent(props){
+function MyCustomComponent(props){
+    
+    const[companyData,setCompanyData]=useState([]);
+
 
     const name = props.name;
     const company = props.company;
     const myStyle={ color:'green',fontSize: '25px'};
     
+    async function getCompanyData(){
+        const url='https://fake-json-api.mock.beeceptor.com/companies';
+        /* fetch(url)
+           .then((res)=>{
+               res.json().then((company)=>{
+                console.log(company)
+               })
+           })
+           .catch((err)=>{
+                console.log(err);
+           }) */
+
+                
+                try {
+                  const response = await fetch(url);
+                  const company = await response.json();
+                  console.log(company);
+                  setCompanyData(company);
+                } catch (error) {
+                  console.error(error.message);
+                }                 
+    }
+    useEffect(()=>{
+        getCompanyData();
+    },[]);
     return(
         <div>
             <h1 style={myStyle}>I am a component,MY name is {name} and my company name is {company}</h1>
@@ -39,9 +68,34 @@ function myCustomComponent(props){
                     
 
                 </table>
+              
+                <div>
+                    <h2>CompanyData Table</h2>
+                    <table border="1">
+                        <thead>
+                           <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Domain</th>
+                            </tr>  
+                        </thead>
+                        <tbody>
+                        {
+                        companyData.map((item)=>(
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td>{item.domain}</td>
+                            </tr>
+                        ))
+                        }
+                        </tbody>
+                    </table>
+                    
+                </div>
             </div>
         </div>
     );
 }
 
-export default myCustomComponent;
+export default MyCustomComponent;
