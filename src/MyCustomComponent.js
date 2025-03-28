a696a1630a92bc7afb2a66a52453bc566925f349
Shakya <varsha.shakya@capgemini.com>
2025-03-28 11:52:42 +0530

    Updated Get and Send API

--- src/MyCustomComponent.js
+++ src/MyCustomComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState , useEffect } from 'react';
+import axios, { Axios } from 'axios';
 
 const fruits=[
     {name:'Guava', price:50 },
@@ -10,14 +11,15 @@ const fruits=[
 function MyCustomComponent(props){
     
     const[companyData,setCompanyData]=useState([]);
-
+    const[newCompany ,setNewCompany]=useState(null);
 
     const name = props.name;
     const company = props.company;
     const myStyle={ color:'green',fontSize: '25px'};
-    
+
+    // fetching data from API
     async function getCompanyData(){
-        const url='https://fake-json-api.mock.beeceptor.com/companies';
+        const url='https://jsonplaceholder.typicode.com/posts';
         /* fetch(url)
            .then((res)=>{
                res.json().then((company)=>{
@@ -29,17 +31,44 @@ function MyCustomComponent(props){
            }) */
 
                 
-                try {
+                /* try {
                   const response = await fetch(url);
                   const company = await response.json();
                   console.log(company);
                   setCompanyData(company);
                 } catch (error) {
                   console.error(error.message);
-                }                 
+                }  */   
+            try{
+                const response =await axios.get(url); 
+                console.log(response.data);
+                setCompanyData(response.data);      
+            } catch(err){
+                console.log(err);
+            }
+              
     }
+
+    //send data to API using axios
+    async function sendData(){
+        const url='https://jsonplaceholder.typicode.com/posts';
+        try{
+             const newEntry={
+                id:companyData.length+1,
+                userId: '1',
+                title: "id minus libero illum nam ad officiis",
+                }
+             const response = await axios.post(url,newEntry);
+             console.log(response.data); 
+             setCompanyData((prevData)=>[...prevData,response.data]);
+             setNewCompany(response.data);
+        }catch(err){
+            console.log(err);
+        }
+    } 
     useEffect(()=>{
         getCompanyData();
+        
     },[]);
     return(
         <div>
@@ -70,13 +99,13 @@ function MyCustomComponent(props){
                 </table>
               
                 <div>
-                    <h2>CompanyData Table</h2>
+                    <h2>Data Table</h2>
                     <table border="1">
                         <thead>
                            <tr>
                             <th>ID</th>
-                            <th>Name</th>
-                            <th>Domain</th>
+                            <th>UserId</th>
+                            <th>Title</th>
                             </tr>  
                         </thead>
                         <tbody>
@@ -84,14 +113,26 @@ function MyCustomComponent(props){
                         companyData.map((item)=>(
                             <tr key={item.id}>
                                 <td>{item.id}</td>
-                                <td>{item.name}</td>
-                                <td>{item.domain}</td>
+                                <td>{item.userId}</td>
+                                <td>{item.title}</td>
                             </tr>
                         ))
                         }
                         </tbody>
                     </table>
                     
+                </div>
+                <div>
+                    <h1>Send Data</h1>
+                    <button onClick={sendData}>Send Data</button>
+                    {newCompany && (
+                       <div>
+                          <h3>New Data</h3>
+                        <p>ID:{newCompany.id}</p>
+                        <p>userID:{newCompany.userId}</p>
+                        <p>Domain:{newCompany.title}</p>
+                       </div>
+                    )}
                 </div>
             </div>
         </div>
